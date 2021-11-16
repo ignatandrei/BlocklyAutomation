d0ecdf5591484fb5569f9873e25e33a2d0d34f64
Andrei Ignat <ignatandrei@yahoo.com>
2021-11-16 20:25:29 +0200

    feat(swagger): delete

--- src/BlocklyHelpers/interpreterHelper.js
+++ src/BlocklyHelpers/interpreterHelper.js
@@ -282,7 +282,45 @@ exports.createInterpreter = function(workspace,BlocklyJavaScript){
     req.send(data);
   }
 ,
-    
+doDelete : (href, callback) => {
+  var req = new XMLHttpRequest();
+
+  req.open('DELETE', href, true);
+  //req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  req.onreadystatechange = function () {
+      if (req.readyState == 4) {
+          if (req.status >= 200 && req.status < 300) {
+              var answer = JSON.stringify({
+                  'origHref': href,
+                  'objectToSend': '',
+                  'status': req.status,
+                  'statusOK': true,
+                  'text': req.responseText
+
+              });
+              return callback(answer);
+
+
+          } else {
+              var answer = JSON.stringify({
+                  'origHref': href,
+                  'objectToSend': '',
+                  'status': req.status,
+                  'statusOK': false,
+                  'text': req.responseText
+
+              });
+              return callback(answer);
+
+          }
+      }
+      else {
+          //window.alert(`error ${href} ${req.status}`);
+      }
+  };
+  req.send(null);
+}, 
    doPost : function (href, objectToPost, callback) {
     let data = objectToPost;
     // console.log(`sending ${data}`);
@@ -452,6 +490,29 @@ exports.createInterpreter = function(workspace,BlocklyJavaScript){
             interpreter.setProperty(globalObject, 'postXhr',
                 interpreter.createAsyncFunction(wrapper));
 
+                var wrapper = (href, callback) => {
+                  try {
+                      var heads = interpreter.pseudoToNative(headersForDomain);
+                      var hostname = '(localSite)';
+                      if (href.startsWith('http://') || href.startsWith('https://')) {
+                          hostname = (new URL(href)).hostname;
+                      }
+                      var arrHeaders = [];
+                      if (hostname in heads) {
+                          arrHeaders = heads[hostname];
+                      }
+                      thisClass.doDelete(href, callback);
+                  }
+                  catch (e) {
+                      alert("is an error" + e);
+                  }
+              };
+  
+  
+  
+              interpreter.setProperty(globalObject, 'deleteXhr',
+                  interpreter.createAsyncFunction(wrapper));
+  
             
                 var wrapper = (href, objectToPost, callback) => {
                   try {
