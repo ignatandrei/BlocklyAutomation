e0c43024385059c065b1e0985644fd0b19009ccb
Andrei Ignat <ignatandrei@yahoo.com>
2021-11-19 07:09:19 +0200

    feat(gui): added demos links

--- src/AutoBlocklyApp/src/app/primary-navig/primary-navig.component.ts
+++ src/AutoBlocklyApp/src/app/primary-navig/primary-navig.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { LoadShowUsageService } from '../load-show-usage.service';
+import { DemoBlocks } from '../DemoBlocks';
 
 @Component({
   selector: 'app-primary-navig',
   templateUrl: './primary-navig.component.html',
   styleUrls: ['./primary-navig.component.css']
 })
-export class PrimaryNavigComponent {
+export class PrimaryNavigComponent implements OnInit {
 
+  demoBlocks: DemoBlocks[]=[];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private loadDemo: LoadShowUsageService) {}
+  ngOnInit(): void {
+    this.loadDemo.getDemoBlocks().subscribe(res => {
+      this.demoBlocks = res.sort((a, b) => a.description.localeCompare(b.description));
+    });
+  }
 
 }
