eb43cbc1b985dc0d10aae65e28af75e79db31934
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-06 22:39:15 +0200

    feat(deploy): v2021.12.6.2238

--- src/AutoBlocklyApp/src/app/Settings.ts
+++ src/AutoBlocklyApp/src/app/Settings.ts
@@ -3,7 +3,7 @@ import { TourSteps } from "./TourSteps";
 
 export class Settings {
     public title: string = '';
-    public static version: string = '2021.12.6.928';
+    public static version: string = '2021.12.6.2238';
     public startBlocks: string[] = [];
     public tourSteps: TourSteps[] = [];
 
