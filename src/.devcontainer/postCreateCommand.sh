51cd85f2ef294f5804f3eb16dd4d8b8b54ebe8e0
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-11 19:02:27 +0200

    fix(dev): install local cli

--- src/.devcontainer/postCreateCommand.sh
+++ src/.devcontainer/postCreateCommand.sh
@@ -1,8 +1,8 @@
 echo 'a'
 npm install -g npm@8.3.0
-npm i -g @angular/cli@13
+npm install @angular/cli@13
 npm i -force
 cd ./AutoBlocklyApp/ 
 npm i -force
 cd ..
-echo 'npm run start'
+echo 'now : npm run start'
