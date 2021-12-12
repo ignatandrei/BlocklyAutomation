7b455f2a3c85165a8c572d43999938169038abc2
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-12 16:31:41 +0200

    feat(local): start having local api

---
+++ src/Local/LocalAPI/LocalAPIChrome/ChromeBookmark.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace LocalAPIChrome;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+public class ChromeBookmark
+{
+    public string date_added { get; set; }
+    public string guid { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
+    public string type { get; set; }
+    public string url { get; set; }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
---
+++ src/Local/LocalAPI/LocalAPIChrome/ChromeBookmarks.cs
@@ -0,0 +1,73 @@
+﻿namespace LocalAPIChrome;
+public class ChromeBookmarks
+{
+    public async Task<ChromeBookmark[]?> GetUrls()
+    {
+        switch (Environment.OSVersion.Platform)
+        {
+            case PlatformID.Win32NT:
+                break;
+            default:
+                throw new ArgumentException($"not yet ready for "+Environment.OSVersion.Platform);
+        }
+
+        var local= Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var chromePathBookmark = Path.Combine(local, "Google", "Chrome", "User Data", "Default");
+        chromePathBookmark = Path.Combine(chromePathBookmark, "Bookmarks");
+        if (!File.Exists(chromePathBookmark))
+            throw new FileNotFoundException(chromePathBookmark);
+
+        var data = await File.ReadAllTextAsync(chromePathBookmark);
+        var node = JsonNode.Parse(data);
+        JsonNode? roots = node["roots"];
+        if (roots == null)
+            return null;
+        JsonNode? bookmark_bar = roots["bookmark_bar"];
+        if (bookmark_bar == null)
+            return null;
+        return parseRecursive(bookmark_bar);
+
+     }
+
+    private ChromeBookmark[] parseRecursive(JsonNode node)
+    {
+        var result = new List<ChromeBookmark>();
+        if (node is JsonArray )
+        {
+            var arr = node.AsArray();
+            //verify it is array
+            if (arr != null && arr.Count > 0)
+            {
+                foreach (var child in arr)
+                {
+                    var data = parseRecursive(child);
+                    result.AddRange(data.Where(it => it != null).ToArray());
+                }
+                return result.ToArray();
+            }
+        }
+
+        
+        JsonNode? children = node["children"];
+        //verify it has children
+        if(children != null)
+        {
+            var data=parseRecursive(children);
+            result.AddRange(data.Where(it=>it != null).ToArray());
+            return result.ToArray();
+        }
+        
+        //try to deserialize 
+        try
+        {
+            var res= node.Deserialize<ChromeBookmark>();
+            result.Add(res);
+            return result.ToArray();
+        }
+        catch (Exception ex)
+        {
+            //todo: log
+        }
+        return result.ToArray();
+    }
+}
