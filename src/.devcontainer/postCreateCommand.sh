923d782051b215e9e85b692ed4772a83040e0770
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-12 08:34:48 +0200

    fix(dev): install

--- src/.devcontainer/postCreateCommand.sh
+++ src/.devcontainer/postCreateCommand.sh
@@ -1,12 +1,14 @@
-echo 'a'
+echo 'install'
 npm install -g npm@8.3.0
+npm install -g ngx-pwa-icons
+npm install -g http-server
 npm install @angular/cli@13
 npm i -force
 cd ./AutoBlocklyApp/ 
 npm i -force
+
 cd ..
-npm install --global http-server
-npm install -g ngx-pwa-icons
+
 echo 'now : npm run start'
 echo 'now : npm run addPWA'
 echo 'now : npm run PWArun'
