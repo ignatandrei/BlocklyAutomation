2f6563b4f0669323b25bead9cf59840408a00c6f
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-12 02:23:24 +0200

    feat(pwa): added display pwa vs browser

--- src/AutoBlocklyApp/src/app/primary-navig/primary-navig.component.ts
+++ src/AutoBlocklyApp/src/app/primary-navig/primary-navig.component.ts
@@ -25,13 +25,18 @@ export class PrimaryNavigComponent implements OnInit {
     public latestVersion:string|undefined='';
     public title:string = "Blockly Automation";
     public footer:string = '';
+    public standalone:boolean=false;
+
   constructor(private breakpointObserver: BreakpointObserver, private details: AppDetails, private ta :TransmitAction) {}
   ngOnInit(): void {
       this.latestVersion = this.details.settings?.latestVersion;
       this.demoBlocks =this.details.demoBlocks.sort((a, b) => a.description.localeCompare(b.description));
       this.title=this.details?.settings?.title ||'Blockly Automation';
       this.footer = this.details?.settings?.footer || '';
-    
+      
+      if ((navigator as any).standalone || window.matchMedia('(display-mode: standalone)').matches) {
+        this.standalone=true; 
+      }
   }
   executeOnDisplayBlockly(func: string){
     this.ta.sendDataToServer('DisplayBlocklyComponent',func);
