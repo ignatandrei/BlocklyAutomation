c2dfb30e7496e65104465ad098a7f77862433039
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-13 18:59:08 +0200

    feat(localapi): email

--- src/Local/LocalAPI/LocalAPIEmail/EmailSender.cs
+++ src/Local/LocalAPI/LocalAPIEmail/EmailSender.cs
@@ -3,14 +3,26 @@ namespace LocalAPIEmail;
 
 public class EmailSender
 {
-    public async Task<bool> Send(EmailData email)
+    private readonly IFluentEmail email;
+
+    public EmailSender(IFluentEmail email)
     {
-        var sendData = Email
-    .From(email.from)
-    .To(email.to)
-    .Subject(email.subject)
-    .UsingTemplate(email.body, email.ObjectToInterpret);
-        var data = await sendData.SendAsync();
+        this.email = email;
+    }
+    public async Task<bool> Send(EmailData emaildata)
+    {
+        if (emaildata == null)
+            return false;
+        ;
+        var e = email;
+        if (!string.IsNullOrWhiteSpace(emaildata.from))
+            e = e.SetFrom(emaildata.from);
+        e = e
+        .To(emaildata.to)
+        .Subject(emaildata.subject)
+        .UsingTemplate(emaildata.body, emaildata.ObjectToInterpret);
+        
+        var data = await e.SendAsync();
         //add logging
         return data.Successful;
     }
