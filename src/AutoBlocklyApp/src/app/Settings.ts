a429b645e820213f574521ba8a2ed6c8079e8761
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-20 07:32:01 +0200

    feat(deploy): going to windows to build wpf

--- src/AutoBlocklyApp/src/app/Settings.ts
+++ src/AutoBlocklyApp/src/app/Settings.ts
@@ -4,7 +4,7 @@ import { TourSteps } from "./TourSteps";
 export class Settings {
     public title: string = '';
     public footer: string = '';
-    public static version: string = '2021.12.20.705';
+    public static version: string = '2021.12.20.708';
     public startBlocks: string[] = [];
     public tourSteps: TourSteps[] = [];
     public latestVersion: string = '';
