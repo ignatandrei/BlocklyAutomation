41eca32bd94e41ee0cadd563f6d36b6d026ec402
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-29 12:58:47 +0200

    feat(localapi): added environment

---
+++ src/Local/LocalAPI/LocalAPI/Controllers/EnvironmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalAPI.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]
+    [AutoActions(template = TemplateIndicator.AllPostWithRecord, FieldsName = new[] { "*" }, ExcludeFields = new[] { "_logger" })]
+    public partial class EnvironmentController : ControllerBase
+    {
+        private readonly ISystem_Environment system_Environment;
+
+        public EnvironmentController(ISystem_Environment system_Environment)
+        {
+            this.system_Environment = system_Environment;
+        }
+    }
+}
