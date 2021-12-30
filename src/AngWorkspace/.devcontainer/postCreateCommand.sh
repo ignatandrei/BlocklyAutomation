741c44e4eab16f035e84f5868ea99a93b467fdcc
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-30 14:17:32 +0200

    feat(deploy): start making ci cid

---
+++ src/AngWorkspace/.devcontainer/postCreateCommand.sh
@@ -0,0 +1,16 @@
+echo 'install'
+npm install -g npm@8.3.0
+# npm install -g ngx-pwa-icons
+npm install -g http-server
+
+npm install @angular/cli@13
+#npm i -force
+#cd ./AutoBlocklyApp/ 
+#npm i -force
+
+#cd ..
+
+echo 'now : npm run start'
+echo 'now : npm run addPWA'
+echo 'now : npm run PWArun'
+echo 'ngx-pwa-icons from angular site'
