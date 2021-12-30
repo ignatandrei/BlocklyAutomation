453ba2da31d16fc0faef1fd99b62b62cb207dab8
Andrei Ignat <ignatandrei@yahoo.com>
2021-12-30 15:37:54 +0200

    fix(ci): v2021.12.30.1530

--- src/AngWorkspace/projects/auto-blockly-app/src/app/Settings.ts
+++ src/AngWorkspace/projects/auto-blockly-app/src/app/Settings.ts
@@ -4,7 +4,7 @@ import { TourSteps } from "./TourSteps";
 export class Settings {
     public title: string = '';
     public footer: string = '';
-    public static version: string = '2021.12.30.1430';
+    public static version: string = '2021.12.30.1530';
     public startBlocks: string[] = [];
     public tourSteps: TourSteps[] = [];
     public latestVersion: string = '';
