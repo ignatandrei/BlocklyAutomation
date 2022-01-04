50d968bdb2810b0d83c8b45dba1a68731d52ecdd
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-05 00:07:18 +0200

    fix(workspace): fix #38 with a hack

---
+++ src/AngWorkspace/fix#38.ps1
@@ -0,0 +1,13 @@
+$arr = Get-ChildItem -Path . -Filter blockly_compressed.js -r | %{$_.FullName}
+#Write-Host $arr
+$arr | %{
+Write-Host "modif	" $_
+$c = Get-Content -path $_
+
+$oldGet= "Blockly.DropDownDiv.getContentDiv=function(){return Blockly.DropDownDiv.content_};"
+$newGet = "Blockly.DropDownDiv.getContentDiv=function(){  var z=Blockly.DropDownDiv.content_;if(!z){   Blockly.DropDownDiv.DIV_=false;Blockly.DropDownDiv.createDom();}return Blockly.DropDownDiv.content_;};"
+
+$c = $c.replace( $oldGet, $newGet)
+$c | Set-Content $_
+}
+Write-Host "delete the .angular folder and ng serve again"
