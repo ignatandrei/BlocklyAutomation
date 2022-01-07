4f3e36a9e38c808da33d54193fa8cc46eadaeeff
Andrei Ignat <ignatandrei@yahoo.com>
2022-01-07 19:01:05 +0200

    feat(local): added powershell

---
+++ src/Local/LocalAPI/LocalTools/PowershellTools.cs
@@ -0,0 +1,40 @@
+﻿namespace LocalTools
+{
+
+    public class LocalTools
+    {
+
+        public async Task<string?> PowershellProfile()
+        {
+            var location = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var strFolder = Path.Combine(location, "WindowsPowerShell");
+            if (!Directory.Exists(strFolder))
+                return null;
+            var file = Path.Combine(strFolder, "Microsoft.PowerShell_profile.ps1");
+            if (!File.Exists(file))
+                return null;
+
+            return await File.ReadAllTextAsync(file));
+
+        }
+        public string?[]? PowershellModules()
+        {
+            var location = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var strFolder = Path.Combine(location, "WindowsPowerShell");
+            if (!Directory.Exists(strFolder))
+                return null;
+            strFolder = Path.Combine(location, "Modules");
+            if (!Directory.Exists(strFolder))
+                return null;
+
+            return Directory
+                .GetDirectories(strFolder)
+                .Select(it=>Path.GetDirectoryName(it))
+                .ToArray()
+                ;
+
+
+
+        }
+    }
+}
