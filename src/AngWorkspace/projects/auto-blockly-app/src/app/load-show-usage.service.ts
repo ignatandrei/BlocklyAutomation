26e48f065861aa4755fe0b35d51f74c516d2d2fb
Andrei Ignat <ignatandrei@yahoo.com>
2022-02-12 21:15:26 +0200

    feat(library): no wrong environment

--- src/AngWorkspace/projects/auto-blockly-app/src/app/load-show-usage.service.ts
+++ src/AngWorkspace/projects/auto-blockly-app/src/app/load-show-usage.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {  Observable, of } from 'rxjs';
 import { map,  tap } from 'rxjs/operators';
 
-import { environment } from '../environments/environment';
 import { DemoBlocks } from "projects/node2-blockly/src/lib/DemoBlocks";
 import { LinksSwagger } from 'projects/node2-blockly/src/lib/LinksSwagger';
 
