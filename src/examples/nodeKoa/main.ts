c3505deb9d30d2fa0391af8d4fd8d2443f42f8e6
Andrei Ignat <ignatandrei@yahoo.com>
2022-02-19 21:05:40 +0200

    feat(example): koa serving from a folder #83

--- src/examples/nodeKoa/main.ts
+++ src/examples/nodeKoa/main.ts
@@ -7,12 +7,22 @@ import errorHandle from './middleware/errorHandle';
 import router from './routes/index';
 
 const serve = require('koa-static');
-
+const send = require('koa-send');
 const app = new Koa();
 
 app
   .use(cors())
+  .use(async (ctx, next) => {
+    if(ctx.path === '/main') {
+      await send(ctx, 'tet/index.html');
+      return;
+    }
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  })
   .use(serve('.'))
+  .use(serve("main",{index: "index.html"}))
   .use(bodyParser())
   .use(errorHandle())
   .use(router.routes())
