d7914e6c77e1de4f518a79078c9bdff1e954def0
Andrei Ignat <ignatandrei@yahoo.com>
2022-02-19 21:27:39 +0200

    feat(example): koa #83  serving all files

--- src/examples/nodeKoa/main.ts
+++ src/examples/nodeKoa/main.ts
@@ -6,17 +6,32 @@ import config from './config';
 import errorHandle from './middleware/errorHandle';
 import router from './routes/index';
 
+
 const serve = require('koa-static');
 const send = require('koa-send');
 const app = new Koa();
-
+const path=require('path');
 app
   .use(cors())
   .use(async (ctx, next) => {
-    if(ctx.path === '/main') {
-      await send(ctx, 'tet/index.html');
+    if(ctx.path === '/BlocklyAutomation' || ctx.path === '/BlocklyAutomation/') {
+      var folder= "tet";
+      console.log('root'+folder);
+      var file=path.join(folder, 'index.html');
+      await send(ctx, file);
       return;
     }
+    if(ctx.path.startsWith('/BlocklyAutomation') ) {
+      
+      var fileToServe = ctx.path.replace('/BlocklyAutomation','');
+      
+      var folder= "tet";
+      var file = path.join(folder, fileToServe);
+      console.log('serving ' + file);
+      await send(ctx, file);
+      return;
+    }
+    
     await next();
     const rt = ctx.response.get('X-Response-Time');
     console.log(`${ctx.method} ${ctx.url} - ${rt}`);
