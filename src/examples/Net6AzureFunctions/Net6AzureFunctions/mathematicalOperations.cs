f3a1049c5e7a8a014134ae9401b181d4e1dd8ac6
Andrei Ignat <ignatandrei@yahoo.com>
2022-05-14 10:23:53 +0300

    feat: substract and dates

--- src/examples/Net6AzureFunctions/Net6AzureFunctions/mathematicalOperations.cs
+++ src/examples/Net6AzureFunctions/Net6AzureFunctions/mathematicalOperations.cs
@@ -22,4 +22,31 @@ public static class mathematicalOperations
 
         return new OkObjectResult(x+y);
     }
+    [FunctionName("substract")]
+    public static ActionResult<int> substract(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "substract/{x:int}/{y:int}")] HttpRequest req,
+        ILogger log, int x, int y)
+    {
+       
+        return new OkObjectResult(x - y);
+    }
+    [FunctionName("testDate")]
+    public static ActionResult<DateTime> addToDateDays(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "testDate/{x:date}/{y:int}")] HttpRequest req,
+        ILogger log, DateTime x, int y)
+    {
+        //log.LogInformation("C# HTTP trigger function processed a request.");
+
+        //string name = req.Query["name"];
+
+        //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        //dynamic data = JsonConvert.DeserializeObject(requestBody);
+        //name = name ?? data?.name;
+
+        //string responseMessage = string.IsNullOrEmpty(name)
+        //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+        return new OkObjectResult(x.AddDays( y));
+    }
 }
