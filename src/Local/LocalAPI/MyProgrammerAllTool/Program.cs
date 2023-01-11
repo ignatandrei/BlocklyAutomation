5c0d836c9d21741dcee52e4cd46af2f6a26be05c
Andrei Ignat <ignatandrei@yahoo.com>
2023-01-11 18:39:01 +0200

    added entry location

--- src/Local/LocalAPI/MyProgrammerAllTool/Program.cs
+++ src/Local/LocalAPI/MyProgrammerAllTool/Program.cs
@@ -1,1 +1,8 @@
-﻿await LocalAPIStarter.Main(null);
+﻿using System.Reflection;
+
+var folder = Assembly.GetExecutingAssembly().Location;
+Console.WriteLine($"Executing {folder}");
+var entry= Assembly.GetEntryAssembly();
+folder = (entry == null) ? "N/A" : entry.Location; 
+Console.WriteLine($"Entry {folder}");
+await LocalAPIStarter.Main(null);
--- src/Local/LocalAPI/README.md
+++ src/Local/LocalAPI/README.md
@@ -1,3 +1,7 @@
 Install as .NET Tool ( local or global)
+```code
+dotnet tool update --global programmerall 
+programerall
+```
 
 Browse to http://localhost:37283/BlocklyAutomation
