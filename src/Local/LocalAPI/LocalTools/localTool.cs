460ad0d3ec29aa31291beae7491673e4478db310
Andrei Ignat <ignatandrei@yahoo.com>
2023-01-12 05:36:36 +0200

    adding dotnet tool

---
+++ src/Local/LocalAPI/LocalTools/localTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalTools;
+
+public class LocalTool
+{
+    public int version { get; set; }
+    public bool isRoot { get; set; }
+    public Dictionary<string,LocalToolData>? tools { get; set; }
+}
+
+public class LocalToolData
+{
+    public string? version { get; set; }
+    public List<string>? commands { get; set; }
+}
+
