8b36792421b9b807120740b44de62bf7e0bdce76
Andrei Ignat <ignatandrei@yahoo.com>
2023-03-16 18:44:35 +0200

    upadte versio

--- src/DeployersToPackage/DockerExtension/deploy.bat
+++ src/DeployersToPackage/DockerExtension/deploy.bat
@@ -1,2 +1,2 @@
 echo find version
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t ignatandrei/blockly-automation:0.0.6 --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t ignatandrei/blockly-automation:0.0.7 --push .
